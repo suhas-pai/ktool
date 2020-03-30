aa2bce4cd8948a47f239b380b12181b2a8561da8
suhas-pai <suhaspai64@gmail.com>
2020-03-29 19:41:38 -0700

    Initial Commit

---
+++ mtool/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mtool
+//
+//  Created by Suhas Pai on 3/29/20.
+//  Copyright © 2020 Suhas Pai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
