ced037f191657fd01796df648c8129c039a734e3
suhas-pai <suhaspai64@gmail.com>
2020-07-19 19:56:54 -0700

    Reverse inheritance order of MemoryMap and ConstMemoryMap

--- include/ADT/MemoryMap.h
+++ include/ADT/MemoryMap.h
@@ -13,26 +13,60 @@
 
 #include "RelativeRange.h"
 
-struct ConstMemoryMap;
-struct MemoryMap {
+struct MemoryMap;
+struct ConstMemoryMap {
 protected:
-    uint8_t *Begin;
-    uint8_t *End;
+    const uint8_t *Begin;
+    const uint8_t *End;
 public:
-    explicit MemoryMap(uint8_t *Begin, uint8_t *End) noexcept;
+    explicit ConstMemoryMap(const uint8_t *Begin, const uint8_t *End) noexcept;
+    explicit ConstMemoryMap(const MemoryMap &Map) noexcept;
 
-    [[nodiscard]] inline uint8_t *getBegin() const noexcept { return Begin; }
-    [[nodiscard]] inline uint8_t *getEnd() const noexcept { return End; }
+    [[nodiscard]] inline const uint8_t *getBegin() const noexcept {
+        return Begin;
+    }
+
+    [[nodiscard]] inline const uint8_t *getEnd() const noexcept {
+        return End;
+    }
+
     [[nodiscard]] inline uint64_t size() const noexcept {
         return (End - Begin);
     }
 
-    inline operator ConstMemoryMap &() noexcept {
-        return reinterpret_cast<ConstMemoryMap &>(*this);
+    template <typename T>
+    [[nodiscard]] inline const T *getBeginAs() const noexcept {
+        return reinterpret_cast<const T *>(getBegin());
     }
 
-    inline operator const ConstMemoryMap &() const noexcept {
-        return reinterpret_cast<const ConstMemoryMap &>(*this);
+    template <typename T>
+    [[nodiscard]] inline const T *getEndAs() const noexcept {
+        return reinterpret_cast<const T *>(getEnd());
+    }
+
+    inline operator MemoryMap &() noexcept {
+        return reinterpret_cast<MemoryMap &>(*this);
+    }
+
+    inline operator const MemoryMap &() const noexcept {
+        return reinterpret_cast<const MemoryMap &>(*this);
+    }
+
+    [[nodiscard]] inline RelativeRange getRange() const noexcept {
+        return RelativeRange(size());
+    }
+};
+
+struct MemoryMap : public ConstMemoryMap {
+public:
+    explicit MemoryMap(uint8_t *Begin, uint8_t *End) noexcept;
+
+    [[nodiscard]] inline uint8_t *getBegin() const noexcept {
+        return const_cast<uint8_t *>(Begin);
+    }
+
+    [[nodiscard]] inline uint8_t *getEnd() const noexcept {
+        return const_cast<uint8_t *>(End);
     }
 
     template <typename T>
@@ -43,31 +77,5 @@ public:
     template <typename T>
     [[nodiscard]] inline T *getEndAs() const noexcept {
         return reinterpret_cast<T *>(getEnd());
-    }
-
-    [[nodiscard]] inline RelativeRange getRange() const noexcept {
-        return RelativeRange(size());
-    }
-};
-
-struct ConstMemoryMap : public MemoryMap {
-public:
-    explicit ConstMemoryMap(const uint8_t *Begin, const uint8_t *End) noexcept;
-    explicit ConstMemoryMap(const MemoryMap &Map) noexcept;
-
-    [[nodiscard]] inline const uint8_t *getBegin() const noexcept {
-        return Begin;
-    }
-
-    [[nodiscard]] inline const uint8_t *getEnd() const noexcept { return End; }
-
-    template <typename T>
-    [[nodiscard]] inline const T *getBeginAs() const noexcept {
-        return reinterpret_cast<const T *>(getBegin());
-    }
-
-    template <typename T>
-    [[nodiscard]] inline const T *getEndAs() const noexcept {
-        return reinterpret_cast<const T *>(getEnd());
     }
 };
--- src/ADT/MemoryMap.cpp
+++ src/ADT/MemoryMap.cpp
@@ -9,12 +9,9 @@
 #include "Utils/Casting.h"
 #include "MemoryMap.h"
 
-MemoryMap::MemoryMap(uint8_t *Begin, uint8_t *End) noexcept
-: Begin(Begin), End(End) {}
-
 ConstMemoryMap::ConstMemoryMap(const uint8_t *Begin,
                                const uint8_t *End) noexcept
-: MemoryMap(const_cast<uint8_t *>(Begin), const_cast<uint8_t *>(End)) {}
+: Begin(Begin), End(End) {}
 
-ConstMemoryMap::ConstMemoryMap(const MemoryMap &Map) noexcept
-: MemoryMap(Map) {}
+MemoryMap::MemoryMap(uint8_t *Begin, uint8_t *End) noexcept
+: ConstMemoryMap(Begin, End) {}
