46becdc0fd122fc9f00541d5b1d75755cefd915c
suhas-pai <suhaspai64@gmail.com>
2020-07-22 02:29:16 -0700

    Revise LoadCommandStorage logic

--- include/ADT/Mach-O/LoadCommandStorage.h
+++ include/ADT/Mach-O/LoadCommandStorage.h
@@ -35,11 +35,11 @@ namespace MachO {
     protected:
         bool IsBigEndian : 1;
     public:
-        explicit LoadCommandStorageIteratorBase(PtrType *Ptr, bool IsBigEndian)
-        noexcept : Base(Ptr), IsBigEndian(IsBigEndian) {};
+        explicit
+        LoadCommandStorageIteratorBase(PtrType *Ptr, bool IsBigEndian) noexcept
+        : Base(Ptr), IsBigEndian(IsBigEndian) {};
 
         using DifferenceType = std::ptrdiff_t;
-
         inline LoadCommandStorageIteratorBase &operator++() noexcept {
             this->Item += SwitchEndianIf(getLoadCmd()->CmdSize, IsBigEndian);
             return *this;
@@ -85,12 +85,12 @@ namespace MachO {
         }
 
         [[nodiscard]]
-            inline bool operator!=(const EndIterator &Iter) const noexcept {
+        inline bool operator!=(const EndIterator &Iter) const noexcept {
             return !(*this == Iter);
         }
     };
 
-    struct LoadCommandStorage {
+    struct ConstLoadCommandStorage {
     public:
         enum class Error : uintptr_t {
             None,
@@ -101,82 +101,6 @@ namespace MachO {
 
             SizeOfCmdsTooSmall,
         };
-    protected:
-        union {
-            uint8_t *Begin;
-            PointerErrorStorage<Error> ErrorStorage;
-        };
-
-        uint8_t *End;
-        uint8_t Ncmds;
-        bool mIsBigEndian : 1;
-
-        LoadCommandStorage(Error Error) noexcept;
-        explicit LoadCommandStorage(uint8_t *Begin,
-                                    uint8_t *End,
-                                    uint32_t Ncmds,
-                                    bool IsBigEndian) noexcept;
-    public:
-        [[nodiscard]] static
-        LoadCommandStorage Open(uint8_t *Begin,
-                                uint32_t Ncmds,
-                                uint32_t SizeOfCmds,
-                                bool IsBigEndian,
-                                bool Is64Bit,
-                                bool Verify) noexcept;
-
-        using Iterator =
-            LoadCommandStorageIteratorBase<uint8_t, MachO::LoadCommand>;
-
-        using ConstIterator =
-            LoadCommandStorageIteratorBase<const uint8_t,
-                                           const MachO::LoadCommand>;
-
-        using EndIterator = LoadCommandStorageEndIteratorBase<uint8_t>;
-        using ConstEndIterator =
-            LoadCommandStorageEndIteratorBase<const uint8_t>;
-
-        [[nodiscard]] inline Iterator begin() const noexcept {
-            return Iterator(Begin, mIsBigEndian);
-        }
-
-        [[nodiscard]] inline EndIterator end() const noexcept {
-            return EndIterator(End);
-        }
-
-        [[nodiscard]] inline ConstIterator cbegin() const noexcept {
-            return ConstIterator(Begin, mIsBigEndian);
-        }
-
-        [[nodiscard]] inline ConstEndIterator cend() const noexcept {
-            return ConstEndIterator(End);
-        }
-
-        [[nodiscard]] inline uint32_t count() const noexcept { return Ncmds; }
-        [[nodiscard]] inline uint32_t size() const noexcept {
-            return static_cast<uint32_t>(End - Begin);
-        }
-
-        [[nodiscard]] inline Error getError() const noexcept {
-            return ErrorStorage.getValue();
-        }
-
-        [[nodiscard]] inline bool hasError() const noexcept {
-            return ErrorStorage.hasValue();
-        }
-
-        [[nodiscard]] inline bool IsBigEndian() const noexcept {
-            return mIsBigEndian;
-        }
-
-        [[nodiscard]] inline uint8_t *getMap() const noexcept {
-            return Begin;
-        }
-    };
-
-    struct ConstLoadCommandStorage {
-    public:
-        using Error = LoadCommandStorage::Error;
     protected:
         union {
             const uint8_t *Begin;
@@ -195,13 +119,13 @@ namespace MachO {
                                 uint32_t Ncmds,
                                 bool IsBigEndian) noexcept;
     public:
-        [[nodiscard]] static
-        ConstLoadCommandStorage Open(const uint8_t *Begin,
-                                     uint32_t Ncmds,
-                                     uint32_t SizeOfCmds,
-                                     bool IsBigEndian,
-                                     bool Is64Bit,
-                                     bool Verify) noexcept;
+        [[nodiscard]] static ConstLoadCommandStorage
+        Open(const uint8_t *Begin,
+             uint32_t Ncmds,
+             uint32_t SizeOfCmds,
+             bool IsBigEndian,
+             bool Is64Bit,
+             bool Verify) noexcept;
 
         using Iterator =
             LoadCommandStorageIteratorBase<const uint8_t,
@@ -247,6 +171,81 @@ namespace MachO {
 
         [[nodiscard]] inline const uint8_t *getMap() const noexcept {
             return Begin;
+        }
+
+        [[nodiscard]] inline const uint8_t *getBegin() const noexcept {
+            return Begin;
+        }
+
+        [[nodiscard]] inline const uint8_t *getEnd() const noexcept {
+            return End;
+        }
+    };
+
+    struct LoadCommandStorage : public ConstLoadCommandStorage {
+    public:
+        using Error = ConstLoadCommandStorage::Error;
+    protected:
+        LoadCommandStorage(Error Error) noexcept;
+
+        explicit
+        LoadCommandStorage(uint8_t *Begin,
+                           uint8_t *End,
+                           uint32_t Ncmds,
+                           bool IsBigEndian) noexcept;
+    public:
+        [[nodiscard]] static inline LoadCommandStorage
+        Open(uint8_t *Begin,
+             uint32_t Ncmds,
+             uint32_t SizeOfCmds,
+             bool IsBigEndian,
+             bool Is64Bit,
+             bool Verify) noexcept
+        {
+            const auto Result =
+                ConstLoadCommandStorage::Open(Begin,
+                                              Ncmds,
+                                              SizeOfCmds,
+                                              IsBigEndian,
+                                              Is64Bit,
+                                              Verify);
+
+            return *reinterpret_cast<const LoadCommandStorage *>(&Result);
+        }
+
+        using Iterator =
+            LoadCommandStorageIteratorBase<uint8_t, MachO::LoadCommand>;
+
+        using ConstIterator =
+            LoadCommandStorageIteratorBase<const uint8_t,
+                                           const MachO::LoadCommand>;
+
+        using EndIterator = LoadCommandStorageEndIteratorBase<uint8_t>;
+        using ConstEndIterator =
+            LoadCommandStorageEndIteratorBase<const uint8_t>;
+
+        [[nodiscard]] inline Iterator begin() const noexcept {
+            return Iterator(getBegin(), sIsBigEndian);
+        }
+
+        [[nodiscard]] inline EndIterator end() const noexcept {
+            return EndIterator(getEnd());
+        }
+
+        [[nodiscard]] inline ConstIterator cbegin() const noexcept {
+            return ConstIterator(getBegin(), sIsBigEndian);
+        }
+
+        [[nodiscard]] inline ConstEndIterator cend() const noexcept {
+            return ConstEndIterator(getEnd());
+        }
+
+        [[nodiscard]] inline uint8_t *getBegin() const noexcept {
+            return const_cast<uint8_t *>(Begin);
+        }
+
+        [[nodiscard]] inline uint8_t *getEnd() const noexcept {
+            return const_cast<uint8_t *>(End);
         }
     };
 }
--- src/ADT/Mach-O/LoadCommandStorage.cpp
+++ src/ADT/Mach-O/LoadCommandStorage.cpp
@@ -9,7 +9,7 @@
 #include "ADT/Mach-O/LoadCommandStorage.h"
 
 namespace MachO {
-    static LoadCommandStorage::Error
+    static ConstLoadCommandStorage::Error
     VerifyLoadCommands(const uint8_t *Begin,
                        const uint8_t *End,
                        uint32_t Ncmds,
@@ -18,7 +18,7 @@ namespace MachO {
                        uint32_t *SizeOfCmdsOut) noexcept
     {
         if (Ncmds == 0) {
-            return LoadCommandStorage::Error::NoLoadCommands;
+            return ConstLoadCommandStorage::Error::NoLoadCommands;
         }
 
         const auto AlignMask = (Is64Bit) ? 7ull : 3ull;
@@ -32,21 +32,21 @@ namespace MachO {
 
             const auto CmdSize = SwitchEndianIf(LoadCmd->CmdSize, IsBigEndian);
             if (CmdSize < sizeof(*LoadCmd)) {
-                return LoadCommandStorage::Error::CmdSizeTooSmall;
+                return ConstLoadCommandStorage::Error::CmdSizeTooSmall;
             }
 
             if (CmdSize & AlignMask) {
-                return LoadCommandStorage::Error::CmdSizeNotAligned;
+                return ConstLoadCommandStorage::Error::CmdSizeNotAligned;
             }
 
             Iter += CmdSize;
             if (Iter > End) {
-                return LoadCommandStorage::Error::SizeOfCmdsTooSmall;
+                return ConstLoadCommandStorage::Error::SizeOfCmdsTooSmall;
             }
 
             if (I != (Ncmds - 1)) {
                 if (Iter == End) {
-                    return LoadCommandStorage::Error::SizeOfCmdsTooSmall;
+                    return ConstLoadCommandStorage::Error::SizeOfCmdsTooSmall;
                 }
 
                 I++;
@@ -56,24 +56,15 @@ namespace MachO {
         } while (true);
 
         if (Iter != End) {
-            return LoadCommandStorage::Error::SizeOfCmdsTooSmall;
+            return ConstLoadCommandStorage::Error::SizeOfCmdsTooSmall;
         }
 
         *SizeOfCmdsOut = static_cast<uint32_t>(Iter - Begin);
-        return LoadCommandStorage::Error::None;
+        return ConstLoadCommandStorage::Error::None;
     }
-
-    LoadCommandStorage::LoadCommandStorage(Error Error) noexcept
-    : ErrorStorage(Error) {}
 
     ConstLoadCommandStorage::ConstLoadCommandStorage(Error Error) noexcept
     : ErrorStorage(Error) {}
-
-    LoadCommandStorage::LoadCommandStorage(uint8_t *Begin,
-                                           uint8_t *End,
-                                           uint32_t Ncmds,
-                                           bool IsBigEndian) noexcept
-    : Begin(Begin), End(End), Ncmds(Ncmds), mIsBigEndian(IsBigEndian) {}
 
     ConstLoadCommandStorage::ConstLoadCommandStorage(const uint8_t *Begin,
                                                      const uint8_t *End,
@@ -81,37 +72,14 @@ namespace MachO {
                                                      bool IsBigEndian) noexcept
     : Begin(Begin), End(End), Ncmds(Ncmds), sIsBigEndian(IsBigEndian) {}
 
-    LoadCommandStorage
-    LoadCommandStorage::Open(uint8_t *Begin,
-                             uint32_t Ncmds,
-                             uint32_t SizeOfCmds,
-                             bool IsBigEndian,
-                             bool Is64Bit,
-                             bool Verify) noexcept
-    {
-        if (SizeOfCmds < sizeof(MachO::LoadCommand)) {
-            return Error::SizeOfCmdsTooSmall;
-        }
+    LoadCommandStorage::LoadCommandStorage(Error Error) noexcept
+    : ConstLoadCommandStorage(Error) {}
 
-        auto End = Begin + SizeOfCmds;
-        if (Verify) {
-            const auto Error =
-                VerifyLoadCommands(Begin,
-                                   End,
-                                   Ncmds,
-                                   IsBigEndian,
-                                   Is64Bit,
-                                   &SizeOfCmds);
-
-            if (Error != Error::None) {
-                return Error;
-            }
-
-            End = Begin + SizeOfCmds;
-        }
-
-        return LoadCommandStorage(Begin, End, Ncmds, IsBigEndian);
-    }
+    LoadCommandStorage::LoadCommandStorage(uint8_t *Begin,
+                                           uint8_t *End,
+                                           uint32_t Ncmds,
+                                           bool IsBigEndian) noexcept
+    : ConstLoadCommandStorage(Begin, End, Ncmds, IsBigEndian) {}
 
     ConstLoadCommandStorage
     ConstLoadCommandStorage::Open(const uint8_t *Begin,
