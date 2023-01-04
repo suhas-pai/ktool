04f9ae55889643014622a3e6457a5e3d97ff5559
Suhas Pai <suhaspai64@gmail.com>
2023-01-04 15:19:34 -0800

    [ADT/FlagsBase] Add new methods, `noexcept`

--- include/ADT/FlagsBase.h
+++ include/ADT/FlagsBase.h
@@ -13,9 +13,13 @@
 #include <limits>
 #include <type_traits>
 
+#include "Utils/Misc.h"
+
 namespace ADT {
     template <std::integral T>
     struct FlagsBase {
+        static_assert(!std::is_signed_v<T>,
+                      "FlagsBase type is a signed integer-type`");
     protected:
         T Flags = 0;
 
@@ -41,8 +45,8 @@ namespace ADT {
             return (Flags & static_cast<T>(Mask));
         }
     public:
-        constexpr FlagsBase() {}
-        constexpr FlagsBase(const T Flags) : Flags(Flags) {}
+        constexpr FlagsBase() noexcept = default;
+        constexpr FlagsBase(const T Flags) noexcept : Flags(Flags) {}
 
         [[nodiscard]]
         constexpr auto operator|(const FlagsBase<T> Mask) const noexcept {
@@ -73,7 +77,25 @@ namespace ADT {
             return Flags == 0;
         }
 
-        [[nodiscard]] constexpr auto Count() const noexcept {
+        [[nodiscard]]
+        constexpr auto FirstSet(const uint8_t Index = 0) const noexcept
+            -> uint8_t
+        {
+            const uint64_t Flags = this->Flags >> Index;
+            if (Flags == 0) {
+                return bit_sizeof(T);
+            }
+
+            if constexpr (sizeof(T) == sizeof(long long)) {
+                return __builtin_ctzll(Flags) + Index;
+            } else if constexpr (sizeof(T) == sizeof(long long)) {
+                return __builtin_ctzl(Flags) + Index;
+            }
+
+            return __builtin_ctz(Flags) + Index;
+        }
+
+        [[nodiscard]] constexpr auto Count() const noexcept -> uint8_t {
             if constexpr (sizeof(T) == sizeof(long long)) {
                 return __builtin_popcountll(Flags);
             } else if constexpr (sizeof(T) == sizeof(long)) {
@@ -81,6 +103,14 @@ namespace ADT {
             }
 
             return __builtin_popcount(Flags);
+        }
+
+        [[nodiscard]]
+        constexpr auto removeFirstBits(const uint8_t BitCount) const noexcept
+            -> decltype(*this)
+        {
+            Flags >>= BitCount;
+            return *this;
         }
 
         [[nodiscard]] constexpr auto value() const noexcept {
