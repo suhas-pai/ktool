ba79caa01289e3592925575af996faece6a2ea2c
Suhas Pai <suhaspai64@gmail.com>
2023-01-20 20:11:06 -0500

    [Objects/Kind] Add APIs

--- include/Objects/Kind.h
+++ include/Objects/Kind.h
@@ -6,7 +6,7 @@
 //
 
 #pragma once
-#include <cstdint>
+#include <string_view>
 
 namespace Objects {
     enum class Kind : uint8_t {
@@ -14,4 +14,30 @@ namespace Objects {
         MachO,
         FatMachO
     };
+
+    [[nodiscard]] constexpr auto KindIsValid(const Kind Kind) noexcept {
+        switch (Kind) {
+            case Kind::None:
+            case Kind::MachO:
+            case Kind::FatMachO:
+                return true;
+        }
+
+        return false;
+    }
+
+    [[nodiscard]] constexpr auto KindGetString(const Kind Kind) noexcept
+        -> std::string_view
+    {
+        switch (Kind) {
+            case Kind::None:
+                assert(false && "Got Kind None in KindGetString()");
+            case Kind::MachO:
+                return "Mach-O";
+            case Kind::FatMachO:
+                return "Fat Mach-O";
+        }
+
+        assert(false && "Got unrecognized Object-Kind in KindGetString()");
+    }
 }
