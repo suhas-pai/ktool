c7e019c19a6026ff4bb41f01088cdbff35cccbac
Suhas Pai <suhaspai64@gmail.com>
2023-01-23 18:50:38 -0500

    [MachO/SegmentList] Re-organize and add new APIs

--- include/MachO/SegmentList.h
+++ include/MachO/SegmentList.h
@@ -14,94 +14,175 @@
 #include "MachO/LoadCommandsMap.h"
 
 namespace MachO {
+    struct SectionInfo {
+        std::string Name;
+
+        uint64_t Addr;
+        uint64_t Size;
+
+        uint32_t FileOffset;
+
+        uint32_t Align;
+        uint32_t RelocFileOffset;
+        uint32_t RelocsCount;
+
+        MachO::SegmentCommand::Section::FlagsStruct Flags;
+
+        uint32_t Reserved1;
+        uint32_t Reserved2;
+        uint32_t Reserved3;
+
+        [[nodiscard]] constexpr auto fileRange() const noexcept {
+            return ADT::Range::FromSize(FileOffset, Size);
+        }
+
+        [[nodiscard]] constexpr auto vmRange() const noexcept {
+            return ADT::Range::FromSize(Addr, Size);
+        }
+    };
+
+    struct SegmentInfo {
+        std::string Name;
+
+        ADT::Range VmRange;
+        ADT::Range FileRange;
+
+        Mach::VmProt MaxProt;
+        Mach::VmProt InitProt;
+
+        MachO::SegmentCommand::FlagsStruct Flags;
+        std::vector<SectionInfo> SectionList;
+
+        [[nodiscard]] constexpr auto
+        findSectionWithName(const std::string_view Name) const noexcept
+            -> const SectionInfo *
+        {
+            for (const auto &Section : SectionList) {
+                if (Section.Name == Name) {
+                    return &Section;
+                }
+            }
+
+            return nullptr;
+        }
+
+        [[nodiscard]] constexpr auto
+        findSectionWithFileOffset(const uint64_t Offset) const noexcept
+            -> const SectionInfo *
+        {
+            for (const auto &Section : SectionList) {
+                if (Section.fileRange().containsLoc(Offset)) {
+                    return &Section;
+                }
+            }
+
+            return nullptr;
+        }
+
+        [[nodiscard]] constexpr auto
+        findSectionWithVmAddr(const uint64_t Offset) const noexcept
+            -> const SectionInfo *
+        {
+            for (const auto &Section : SectionList) {
+                if (Section.vmRange().containsLoc(Offset)) {
+                    return &Section;
+                }
+            }
+
+            return nullptr;
+        }
+    };
+
     struct SegmentList {
-    public:
-        struct SectionInfo {
-            std::string Name;
-
-            uint64_t Addr;
-            uint64_t Size;
-
-            uint32_t FileOffset;
-
-            uint32_t Align;
-            uint32_t RelocFileOffset;
-            uint32_t RelocsCount;
-
-            MachO::SegmentCommand::Section::FlagsStruct Flags;
-
-            uint32_t Reserved1;
-            uint32_t Reserved2;
-            uint32_t Reserved3;
-
-            [[nodiscard]] constexpr auto fileRange() const noexcept {
-                return ADT::Range::FromSize(FileOffset, Size);
-            }
-
-            [[nodiscard]] constexpr auto vmRange() const noexcept {
-                return ADT::Range::FromSize(Addr, Size);
-            }
-        };
-
-        struct SegmentInfo {
-            std::string Name;
-
-            ADT::Range VmRange;
-            ADT::Range FileRange;
-
-            Mach::VmProt MaxProt;
-            Mach::VmProt InitProt;
-
-            MachO::SegmentCommand::FlagsStruct Flags;
-            std::vector<SectionInfo> SectionList;
-
-            [[nodiscard]] constexpr auto
-            GetSectionWithName(const std::string_view Name) const noexcept
-                -> const SectionInfo *
-            {
-                for (const auto &Section : SectionList) {
-                    if (Section.Name == Name) {
-                        return &Section;
-                    }
-                }
-
-                return nullptr;
-            }
-
-            [[nodiscard]] constexpr auto
-            GetSectionWithFileOffset(const uint64_t Offset) const noexcept
-                -> const SectionInfo *
-            {
-                for (const auto &Section : SectionList) {
-                    if (Section.fileRange().containsLoc(Offset)) {
-                        return &Section;
-                    }
-                }
-
-                return nullptr;
-            }
-
-            [[nodiscard]] constexpr auto
-            GetSectionWithVmOffset(const uint64_t Offset) const noexcept
-                -> const SectionInfo *
-            {
-                for (const auto &Section : SectionList) {
-                    if (Section.vmRange().containsLoc(Offset)) {
-                        return &Section;
-                    }
-                }
-
-                return nullptr;
-            }
-        };
     protected:
         std::vector<SegmentInfo> List;
     public:
+        SegmentList() noexcept = default;
+
         explicit
         SegmentList(const MachO::LoadCommandsMap &Map, bool Is64Bit) noexcept;
 
-        [[nodiscard]] constexpr auto
-        GetSegmentWithName(const std::string_view Name) const noexcept
+        constexpr auto
+        addSegment(const MachO::SegmentCommand &Segment,
+                   const bool IsBigEndian) noexcept
+            -> decltype(*this)
+        {
+            auto &Info = List.emplace_back(SegmentInfo {
+                .Name = std::string(Segment.segmentName()),
+                .VmRange =
+                    ADT::Range::FromSize(Segment.vmAddr(IsBigEndian),
+                                         Segment.vmSize(IsBigEndian)),
+                .FileRange =
+                    ADT::Range::FromSize(Segment.fileOffset(IsBigEndian),
+                                         Segment.fileSize(IsBigEndian)),
+                .MaxProt = Segment.maxProt(IsBigEndian),
+                .InitProt = Segment.initProt(IsBigEndian),
+                .Flags = Segment.flags(IsBigEndian),
+                .SectionList = {}
+            });
+
+            for (const auto &Section :
+                    Segment.sectionList(IsBigEndian))
+            {
+                Info.SectionList.emplace_back(SectionInfo {
+                    .Name = std::string(Section.sectionName()),
+                    .Addr = Section.addr(IsBigEndian),
+                    .Size = Section.size(IsBigEndian),
+                    .FileOffset = Section.fileOffset(IsBigEndian),
+                    .Align = Section.align(IsBigEndian),
+                    .RelocFileOffset = Section.relocFileOffset(IsBigEndian),
+                    .RelocsCount = Section.relocsCount(IsBigEndian),
+                    .Flags = Section.flags(IsBigEndian),
+                    .Reserved1 = Section.reserved1(IsBigEndian),
+                    .Reserved2 = Section.reserved2(IsBigEndian),
+                });
+            }
+
+            return *this;
+        }
+
+        constexpr auto
+        addSegment(const MachO::SegmentCommand64 &Segment,
+                   const bool IsBigEndian) noexcept
+            -> decltype(*this)
+        {
+            auto &Info = List.emplace_back(SegmentInfo {
+                .Name = std::string(Segment.segmentName()),
+                .VmRange =
+                    ADT::Range::FromSize(Segment.vmAddr(IsBigEndian),
+                                         Segment.vmSize(IsBigEndian)),
+                .FileRange =
+                    ADT::Range::FromSize(Segment.fileOffset(IsBigEndian),
+                                         Segment.fileSize(IsBigEndian)),
+                .MaxProt = Segment.maxProt(IsBigEndian),
+                .InitProt = Segment.initProt(IsBigEndian),
+                .Flags = Segment.flags(IsBigEndian),
+                .SectionList = {}
+            });
+
+            for (const auto &Section :
+                    Segment.sectionList(IsBigEndian))
+            {
+                Info.SectionList.emplace_back(SectionInfo {
+                    .Name = std::string(Section.sectionName()),
+                    .Addr = Section.addr(IsBigEndian),
+                    .Size = Section.size(IsBigEndian),
+                    .FileOffset = Section.fileOffset(IsBigEndian),
+                    .Align = Section.align(IsBigEndian),
+                    .RelocFileOffset = Section.relocFileOffset(IsBigEndian),
+                    .RelocsCount = Section.relocsCount(IsBigEndian),
+                    .Flags = Section.flags(IsBigEndian),
+                    .Reserved1 = Section.reserved1(IsBigEndian),
+                    .Reserved2 = Section.reserved2(IsBigEndian),
+                    .Reserved3 = Section.reserved3(IsBigEndian)
+                });
+            }
+
+            return *this;
+        }
+
+        [[nodiscard]] virtual auto
+        findSegmentWithName(const std::string_view Name) const noexcept
             -> const SegmentInfo *
         {
             for (const auto &Info : List) {
@@ -113,8 +194,8 @@ namespace MachO {
             return nullptr;
         }
 
-        [[nodiscard]] constexpr auto
-        GetSegmentWithFileOffset(const uint64_t Offset) const noexcept
+        [[nodiscard]] virtual auto
+        findSegmentWithFileOffset(const uint64_t Offset) const noexcept
             -> const SegmentInfo *
         {
             for (const auto &Info : List) {
@@ -126,8 +207,8 @@ namespace MachO {
             return nullptr;
         }
 
-        [[nodiscard]] constexpr auto
-        GetSegmentWithVmAddr(const uint64_t Addr) const noexcept
+        [[nodiscard]] virtual auto
+        findSegmentWithVmAddr(const uint64_t Addr) const noexcept
             -> const SegmentInfo *
         {
             for (const auto &Info : List) {
--- src/MachO/SegmentList.cpp
+++ src/MachO/SegmentList.cpp
@@ -15,39 +15,7 @@ namespace MachO {
                 if (const auto Segment =
                         dyn_cast<MachO::SegmentCommand64>(&LC, IsBigEndian))
                 {
-                    auto &Info = List.emplace_back(SegmentInfo {
-                        .Name = std::string(Segment->segmentName()),
-                        .VmRange =
-                            ADT::Range::FromSize(Segment->vmAddr(IsBigEndian),
-                                                 Segment->vmSize(IsBigEndian)),
-                        .FileRange =
-                            ADT::Range::FromSize(
-                                Segment->fileOffset(IsBigEndian),
-                                Segment->fileSize(IsBigEndian)),
-                        .MaxProt = Segment->maxProt(IsBigEndian),
-                        .InitProt = Segment->initProt(IsBigEndian),
-                        .Flags = Segment->flags(IsBigEndian),
-                        .SectionList = {}
-                    });
-
-                    for (const auto &Section :
-                            Segment->sectionList(IsBigEndian))
-                    {
-                        Info.SectionList.emplace_back(SectionInfo {
-                            .Name = std::string(Section.sectionName()),
-                            .Addr = Section.addr(IsBigEndian),
-                            .Size = Section.size(IsBigEndian),
-                            .FileOffset = Section.fileOffset(IsBigEndian),
-                            .Align = Section.align(IsBigEndian),
-                            .RelocFileOffset =
-                                Section.relocFileOffset(IsBigEndian),
-                            .RelocsCount = Section.relocsCount(IsBigEndian),
-                            .Flags = Section.flags(IsBigEndian),
-                            .Reserved1 = Section.reserved1(IsBigEndian),
-                            .Reserved2 = Section.reserved2(IsBigEndian),
-                            .Reserved3 = Section.reserved3(IsBigEndian)
-                        });
-                    }
+                    addSegment(*Segment, IsBigEndian);
                 }
             }
         } else {
@@ -55,39 +23,7 @@ namespace MachO {
                 if (const auto Segment =
                         dyn_cast<MachO::SegmentCommand>(&LC, IsBigEndian))
                 {
-                    auto &Info = List.emplace_back(SegmentInfo {
-                        .Name = std::string(Segment->segmentName()),
-                        .VmRange =
-                            ADT::Range::FromSize(Segment->vmAddr(IsBigEndian),
-                                                 Segment->vmSize(IsBigEndian)),
-                        .FileRange =
-                            ADT::Range::FromSize(
-                                Segment->fileOffset(IsBigEndian),
-                                Segment->fileSize(IsBigEndian)),
-                        .MaxProt = Segment->maxProt(IsBigEndian),
-                        .InitProt = Segment->initProt(IsBigEndian),
-                        .Flags = Segment->flags(IsBigEndian),
-                        .SectionList = {}
-                    });
-
-                    for (const auto &Section :
-                            Segment->sectionList(IsBigEndian))
-                    {
-                        Info.SectionList.emplace_back(SectionInfo {
-                            .Name = std::string(Section.sectionName()),
-                            .Addr = Section.addr(IsBigEndian),
-                            .Size = Section.size(IsBigEndian),
-                            .FileOffset = Section.fileOffset(IsBigEndian),
-                            .Align = Section.align(IsBigEndian),
-                            .RelocFileOffset =
-                                Section.relocFileOffset(IsBigEndian),
-                            .RelocsCount = Section.relocsCount(IsBigEndian),
-                            .Flags = Section.flags(IsBigEndian),
-                            .Reserved1 = Section.reserved1(IsBigEndian),
-                            .Reserved2 = Section.reserved2(IsBigEndian),
-                            .Reserved3 = 0
-                        });
-                    }
+                    addSegment(*Segment, IsBigEndian);
                 }
             }
         }
